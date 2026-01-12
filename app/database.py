572e88a8a54ddffa56c1e7af7cfdfcbb47ddc5da
yasirverse <yasirverse.ai@gmail.com>
2026-01-12 10:48:26 +0500

    add database .env

--- app/database.py
+++ app/database.py
@@ -3,14 +3,19 @@ from databases import Database
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./database.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("SUPABASE_DB_URL")
+# DATABASE_URL = 'postgresql+psycopg2://postgres:xSNSFV3a0UN1y3bG@aws-0-region.pooler.supabase.com:6543/postgres'
 
 # Database connection
 database = Database(DATABASE_URL)
 
 # SQLAlchemy setup
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
